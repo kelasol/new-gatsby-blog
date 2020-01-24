8b085bd9c5911592573c1d54316642c5f85b1629
kelasol <sp4cet04st@gmail.com>
2020-01-24 14:26:04 -0800

    adding all our new blog code for gatsby

---
+++ src/pages/page-3.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ThirdPage = () => (
+  <Layout>
+    <h1> Hello </h1>
+  </Layout>
+)
+
+export default ThirdPage
